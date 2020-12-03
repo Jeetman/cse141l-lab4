53e8a0ff6271913f86065321162112cbbfe9f066
Akshay <kamal.akshay@att.net>
2020-12-03 15:16:36 -0800

    Rewrote program2 (divide) to fit new spec

--- src/assembler/assembler.py
+++ src/assembler/assembler.py
@@ -8,7 +8,6 @@ def assembly_to_machine(line):
 
     # Grab the first 3 chars of the line, and use LUT to generate opcode
     inst_name= line[:3]
-    # TODO: remove space here
     opcode = lut.LUT[inst_name] 
 
     # Store space-delimited elements of the instruction
@@ -51,8 +50,6 @@ def main():
     with open(results.file_in, 'r') as fi, open(results.file_out, 'w') as fo:
         lines = [line.rstrip() for line in fi]
 
-        # TODO: determine whether or not to include newlines (currently we
-        # don't)
         for inst in lines:
             fo.write(assembly_to_machine(inst))
             fo.write('\n')
